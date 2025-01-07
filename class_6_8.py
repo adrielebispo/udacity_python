385764cb217af238bf0f8357cdcc66e9518230b0
Adriele Bispo <adriele.bispo122@gmail.com>
2025-01-07 10:02:30 -0300

    Udacity python class 6.8

---
+++ class_6_8.py
@@ -0,0 +1,9 @@
+name = input('Enter a name: ')
+print(f'Hello, {name}!')
+
+num = int(input('Enter a number: '))
+num += 20
+print(num)
+
+result = eval(input("Enter an expression: "))
+print(result)
